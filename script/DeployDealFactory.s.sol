f8a766804a5e475dfc3b65772f92c021799af4ef
maxcoto <maxiperezc@gmail.com>
2024-05-22 16:00:25 -0300

    adds a factory for deals

---
+++ script/DeployDealFactory.s.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+
+import "@openzeppelin/contracts/utils/Create2.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+import "../src/DealFactory.sol";
+
+contract DeployDealFactory is Script {
+    function run() external {
+        bytes32 salt = 0x6551655165516551655165516551655165516551655165516551655165516551;
+        address factory = 0x4e59b44847b379578588920cA78FbF26c0B4956C;
+
+        address registry = 0x000000006551c19487814612e58FE06813775758;
+
+        // TODO: change to a cross chain standard address
+        address implementation = 0x83Bd10AE8E626EE9977Eaf222487fFCE60279c30;
+
+        // TODO: change to a multisig
+        address treasury = 0x6049176a7507cC93bafaaC786f4Aa5Fb37707207;
+
+        // TODO: change to tokens.surge.rip
+        string memory baseURI = "https://surgetokens.netlify.app";
+
+        address dealFactory = Create2.computeAddress(
+            salt,
+            keccak256(
+                abi.encodePacked(
+                    type(DealFactory).creationCode,
+                    abi.encode(
+                        registry,
+                        implementation,
+                        treasury,
+                        baseURI
+                    )
+                )
+            ),
+            factory
+        );
+
+        // Deploy Deal
+        if (dealFactory.code.length == 0) {
+            vm.startBroadcast();
+            new DealFactory{salt: salt}(
+                registry,
+                implementation,
+                treasury,
+                baseURI
+            );
+            vm.stopBroadcast();
+
+            console.log("DealFactory:", dealFactory, "(deployed)");
+        } else {
+            console.log("DealNFT:", dealFactory, "(exists)");
+        }
+
+        console.log("\nVerification Commands:\n");
+        console.log(
+            "DealFactory: forge verify-contract --num-of-optimizations 200 --chain-id",
+            block.chainid,
+            dealFactory,
+            string.concat(
+                "src/DealNFT.sol:DealNFT --constructor-args $(cast abi-encode \"constructor(address,address,address,string)\" ",
+                Strings.toHexString(registry),
+                " ",
+                Strings.toHexString(implementation),
+                " ",
+                Strings.toHexString(treasury),
+                " ",
+                baseURI,
+                ")\n"
+            )
+        );
+    }
+}
---
+++ src/DealFactory.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import {DealNFT} from "./DealNFT.sol";
+
+contract DealFactory {
+
+    event Create(address indexed deal, address indexed sponsor, string name, string symbol);
+    event Sponsor(address indexed sponsor, bool approved);
+
+    address private _registry;
+    address private _implementation;
+    address private _treasury;
+    string private _baseURI;
+
+    constructor(
+        address registry_,
+        address implementation_,
+        address treasury_,
+        string memory baseURI_
+    ) {
+        require(registry_ != address(0), "registry is the zero address");
+        require(implementation_ != address(0), "implementation is the zero address");
+        require(treasury_ != address(0), "treasury is the zero address");
+        require(bytes(baseURI_).length > 0, "baseURI is empty");
+
+        _registry = registry_;
+        _implementation = implementation_;
+        _treasury = treasury_;
+        _baseURI = baseURI_;
+    }
+
+    function create(
+        address sponsor_,
+        string memory name_,
+        string memory symbol_
+    ) external returns (address)  {
+        require(sponsor_ != address(0), "sponsor is the zero address");
+        require(bytes(name_).length > 0, "name is empty");
+        require(bytes(symbol_).length > 0, "symbol is empty");
+
+        DealNFT deal = new DealNFT(
+            _registry,
+            _implementation,
+            sponsor_,
+            _treasury,
+            name_,
+            symbol_,
+            _baseURI
+        );
+
+        emit Create(address(deal), sponsor_, name_, symbol_);
+
+        return address(deal);
+    }
+}
